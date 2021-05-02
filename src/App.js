485742c3c26d54cbcb63a348ae391f6382d5f5e6
Mehdi Hasan Shohan <mehdihasanshohan25@gmail.com>
2021-05-03 03:08:44 +0600

    React Startup with ESLint

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import ReactDev from './components/ReactDev';
+
+function App() {
+    return (
+        <div>
+            <ReactDev />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/ReactDev.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ReactDev = () => (
+    <div>
+        <h1 style={{ textAlign: 'center', fontSize: '60px', marginTop: '300px' }}>
+            Hello, React Developer...!!!
+        </h1>
+    </div>
+);
+
+export default ReactDev;
